85d15a8678d0c516c6140b8b72f00f5324a7a47d
Djitue <133507812+Djitue@users.noreply.github.com>
2023-06-23 14:44:08 +0100

    Update appconfig.js

--- config/appconfig.js
+++ config/appconfig.js
@@ -2,7 +2,6 @@ require('dotenv').config;
 
 const MONGO_DB_CONFIG = {
 
-  // DB: 'mongodb+srv://brindadjitue:LxM6LZJfdJmNj3lY@fecluster.y8x2pkl.mongodb.net/ReactFrontendProject?retryWrites=true&w=majority'
   DB : 'mongodb+srv://brindadjitue:qWmEqCUK0mDFp0tA@cluster0.rg1gvhr.mongodb.net/ReactFrontendProject?retryWrites=true&w=majority'
 } 
 module.exports = {
